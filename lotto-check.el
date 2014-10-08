c44d269de27506333f79395b5d93e15750ffb40f
Sang-gi Lee <kaisyu@gmail.com>
2014-10-09 03:51:03 +0900

    * `lotto-gen.el' 파일 포맷을 unix 형식으로 변경 * 버전명 변경

--- lotto-check.el
+++ lotto-check.el
@@ -14,7 +14,7 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-;; Version 0.2.3
+;; Version 0.2.4
 ;; Author: Sang-gi Lee <kaisyu@gmail.com>
 
 ;; Requirements:
